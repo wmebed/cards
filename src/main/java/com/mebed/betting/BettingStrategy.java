dc20089f2d128ffbdcf594bcefbd5f6a8f065dcf
Wade Mebed <wmebed@gmail.com>
2015-01-02 21:39:53 -0800

    Added occasional folds on big bets

--- src/main/java/com/mebed/betting/BettingStrategy.java
+++ src/main/java/com/mebed/betting/BettingStrategy.java
@@ -1,22 +1,34 @@
 package com.mebed.betting;
 
+import java.util.Collections;
+import java.util.Random;
+
 import com.mebed.cards.Hand;
 import com.mebed.cards.PokerGame;
 
 public class BettingStrategy {
 
-	public static Strategy getStrategy(Hand hand, Account account, Account pot) {
+	public static Strategy getStrategy(Hand hand, Account account, Account pot, double opponentBet) {
 		Strategy strategy = new Strategy();
 		HandCategory category = PokerGame.getCategory(hand);
-		if (category.ordinal() > 1) {
-			strategy.setBet(5);
-			strategy.setHandStatus(HandStatus.raise);
-		} else if (hand.getCards().size() == 2) {
-			strategy.setHandStatus(HandStatus.call);
-		} else if (hand.getCards().size()  > 6) {
-			strategy.setHandStatus(HandStatus.fold);
+		if (opponentBet > 10) {
+			long seed = System.nanoTime();
+			if (new Random(seed).nextInt() % 2 == 0) {
+				strategy.setHandStatus(HandStatus.fold);
+			} else {
+				strategy.setHandStatus(HandStatus.call);
+			}
 		} else {
-			strategy.setHandStatus(HandStatus.call);
+			if (category.ordinal() > 1) {
+				strategy.setBet(5);
+				strategy.setHandStatus(HandStatus.raise);
+			} else if (hand.getCards().size() == 2) {
+				strategy.setHandStatus(HandStatus.call);
+			} else if (hand.getCards().size()  > 6) {
+				strategy.setHandStatus(HandStatus.fold);
+			} else {
+				strategy.setHandStatus(HandStatus.call);
+			}
 		}
 		return strategy;
 	}
