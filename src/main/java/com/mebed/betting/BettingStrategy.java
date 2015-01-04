fc4765fbcfb70c0fcf34634e93f046a20ba2939b
Wade Mebed <wmebed@gmail.com>
2015-01-04 10:48:13 -0800

    Fix all in

--- src/main/java/com/mebed/betting/BettingStrategy.java
+++ src/main/java/com/mebed/betting/BettingStrategy.java
@@ -17,43 +17,60 @@ public class BettingStrategy {
 				strategy.setHandStatus(HandStatus.fold);
 			} else {
 				strategy.setHandStatus(HandStatus.call);
-				strategy.setBet(opponentBet);
+				setCallBet(account, strategy, opponentBet);
 			}
 		} else if (opponentBet == 0) {
 			if (new Random(seed).nextInt() % 2 == 0) {
 				strategy.setHandStatus(HandStatus.call);
-				strategy.setBet(opponentBet);
+				setCallBet(account, strategy, opponentBet);
 			} else if (handStatus != HandStatus.call) {
 				strategy.setHandStatus(HandStatus.raise);
-				double amount = Math.abs(new Random(seed).nextInt() % 50);
-				if (amount > account.getBalance()) {
-					amount = account.getBalance();
-				}
-				strategy.setBet(amount + 5);
+				setRaiseBet(account, strategy, seed);
 			} else {
 				strategy.setHandStatus(HandStatus.call);
-				strategy.setBet(opponentBet);
+				setCallBet(account, strategy, opponentBet);
 			}
 		} else {
 			if (category.ordinal() > 1 && handStatus != HandStatus.call) {
 				int amount = Math.abs(new Random(seed).nextInt() % 50);
-				strategy.setBet(amount + 5);
+				setRaiseBet(account, strategy, seed);
 				strategy.setHandStatus(HandStatus.raise);
 			} else if (hand.getCards().size() == 2) {
 				strategy.setHandStatus(HandStatus.call);
-				strategy.setBet(opponentBet);
+				setCallBet(account, strategy, opponentBet);
 			} else if (hand.getCards().size()  > 6 && handStatus == HandStatus.raise) {
 				if (new Random(seed).nextInt() % 2 == 0) {
 					strategy.setHandStatus(HandStatus.fold);
 				} else {
 					strategy.setHandStatus(HandStatus.call);
-					strategy.setBet(opponentBet);
+					setCallBet(account, strategy, opponentBet);
 				}
 			} else {
 				strategy.setHandStatus(HandStatus.call);
-				strategy.setBet(opponentBet);
+				setCallBet(account, strategy, opponentBet);
 			}
 		}
 		return strategy;
 	}
+
+	private static void setRaiseBet(Account account, Strategy strategy,
+			long seed) {
+		double amount = Math.abs(new Random(seed).nextInt() % 50);
+		if (amount > account.getBalance()) {
+			amount = account.getBalance();
+		}
+		strategy.setBet(amount);
+	}
+	
+	private static void setCallBet(Account account, Strategy strategy, Double opponentBet) {
+		Double amount = null;
+		if (opponentBet > account.getBalance()) {
+			amount = account.getBalance();
+		} else {
+			amount = opponentBet;
+		}
+		strategy.setBet(amount);
+	}
+	
+
 }
