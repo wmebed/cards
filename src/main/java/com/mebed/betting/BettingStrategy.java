bc7376472c0d525d4a9f4f7c82a8e5b1038da823
Wade Mebed <wmebed@gmail.com>
2015-01-03 22:28:29 -0800

    Fixed call logic

--- src/main/java/com/mebed/betting/BettingStrategy.java
+++ src/main/java/com/mebed/betting/BettingStrategy.java
@@ -8,11 +8,11 @@ import com.mebed.cards.PokerGame;
 
 public class BettingStrategy {
 
-	public static Strategy getStrategy(Hand hand, Account account, Account pot, double opponentBet) {
+	public static Strategy getStrategy(HandStatus handStatus, Hand hand, Account account, Account pot, double opponentBet) {
 		Strategy strategy = new Strategy();
 		long seed = System.nanoTime();
 		HandCategory category = PokerGame.getCategory(hand);
-		if (opponentBet > 9) {
+		if (opponentBet > 9 && handStatus != HandStatus.call) {
 			if (new Random(seed).nextInt() % 2 == 0) {
 				strategy.setHandStatus(HandStatus.fold);
 			} else {
@@ -21,22 +21,25 @@ public class BettingStrategy {
 		} else if (opponentBet == 0) {
 			if (new Random(seed).nextInt() % 2 == 0) {
 				strategy.setHandStatus(HandStatus.call);
-			} else {
+				strategy.setBet(opponentBet);
+			} else if (handStatus != HandStatus.call) {
 				strategy.setHandStatus(HandStatus.raise);
 				double amount = Math.abs(new Random(seed).nextInt() % 50);
 				if (amount > account.getBalance()) {
 					amount = account.getBalance();
 				}
 				strategy.setBet(amount + 5);
+			} else {
+				strategy.setHandStatus(HandStatus.call);
 			}
 		} else {
-			if (category.ordinal() > 1) {
+			if (category.ordinal() > 1 && handStatus != HandStatus.call) {
 				int amount = Math.abs(new Random(seed).nextInt() % 50);
 				strategy.setBet(amount + 5);
 				strategy.setHandStatus(HandStatus.raise);
 			} else if (hand.getCards().size() == 2) {
 				strategy.setHandStatus(HandStatus.call);
-			} else if (hand.getCards().size()  > 6) {
+			} else if (hand.getCards().size()  > 6 && handStatus == HandStatus.raise) {
 				if (new Random(seed).nextInt() % 2 == 0) {
 					strategy.setHandStatus(HandStatus.fold);
 				} else {
